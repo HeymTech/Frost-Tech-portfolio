59a1e1d42dc7b9b746d5e8f5f71a965c7d4ee34a
HeymTech <heymtech26@gmail.com>
2026-02-25 23:32:11 +0530

    final map fixing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -85,14 +85,7 @@ return (
 
     {/* RIGHT SIDE - MAP */}
     <div className="rounded-xl overflow-hidden shadow-2xl border border-zinc-700">
-      <iframe
-        title="Google Map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.113772381353!2d80.1814873735901!3d13.02842621363286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5260d1569b78c7%3A0x545cee75ef10c83a!2sRasi%20General%20and%20Ortho%20Clinic!5e0!3m2!1sen!2sin!4v1771922797136!5m2!1sen!2sin"
-        width="100%"
-        height="500"
-        style={{ border: 0 }}
-        loading="lazy"
-      ></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.0749061789934!2d80.15389089189564!3d12.904660694420137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525f12ab397073%3A0x269688fb4982abc8!2sKumaran%20flats!5e0!3m2!1sen!2sin!4v1772042404089!5m2!1sen!2sin" width="600" height="450" loading="lazy"></iframe>
     </div>
 
   </div>
